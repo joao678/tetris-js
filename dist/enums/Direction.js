24eb7a4024fa407ef5c06a84bf73b2de2d62ae76
João Vitor Fernandes Canuto <joao678@gmail.com>
2022-10-21 23:13:14 -0300

    dist

--- dist/enums/Direction.js
+++
@@ -1,6 +0,0 @@
-export var Direction;
-(function (Direction) {
-    Direction[Direction["Left"] = 0] = "Left";
-    Direction[Direction["Right"] = 1] = "Right";
-})(Direction || (Direction = {}));
-//# sourceMappingURL=Direction.js.map
